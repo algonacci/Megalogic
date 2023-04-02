76d20f3745d50da18ce257fe276348f0b4807822
Eric Julianto <algonacci1@gmail.com>
2023-04-02 11:30:33 +0700

    Initial Commit

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+export default defineConfig({
+  site: "https://megalogic.vercel.app",
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    mdx(),
+    sitemap(),
+  ],
+});
